efdfbb6a22817ae9ba682bacc9495337d82a5c60
tis-bell-jar <r12583509@gmail.com>
2025-06-20 21:42:32 +0300

    Add color and position classes

--- website/models.py
+++ website/models.py
@@ -1,12 +1,30 @@
-from . import db 
+from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from enum import Enum
+
+
+class Color(Enum):
+    """Available note colors."""
+    RED = "red"
+    GREEN = "green"
+    BLUE = "blue"
+    YELLOW = "yellow"
+
+
+class Position(Enum):
+    """Possible CSS position classes for notes."""
+    LEFT = "left"
+    CENTER = "center"
+    RIGHT = "right"
 
 class Note(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True),default=func.now())
     position = db.Column(db.Integer, default=0)
+    color = db.Column(db.String(20), default=Color.YELLOW.value)
+    position_class = db.Column(db.String(20), default=Position.LEFT.value)
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
 
