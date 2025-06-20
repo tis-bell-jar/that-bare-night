c54882d8adbdcd859807c07d26d77411e3ef32f4
tis-bell-jar <r12583509@gmail.com>
2025-06-20 21:14:26 +0300

    Add draggable note ordering

--- website/models.py
+++ website/models.py
@@ -6,6 +6,7 @@ class Note(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True),default=func.now())
+    position = db.Column(db.Integer, default=0)
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
 
--- website/views.py
+++ website/views.py
@@ -16,15 +16,19 @@ def home():
         if len(note)<1:
             flash('Note is too short',category='error')
         else:
-            new_note = Note(data=note,user_id=current_user.id)
+            # determine next position for the user's notes
+            max_pos = db.session.query(db.func.max(Note.position)).filter_by(user_id=current_user.id).scalar()
+            next_pos = (max_pos or 0) + 1
+            new_note = Note(data=note,user_id=current_user.id, position=next_pos)
             db.session.add(new_note)
             db.session.commit()
             flash('Note added',category='success')
 
-    return render_template("home.html",user=current_user)
+    notes = Note.query.filter_by(user_id=current_user.id).order_by(Note.position).all()
+    return render_template("home.html",user=current_user, notes=notes)
 
 @views.route('/delete-note', methods=['POST'])
-def delete_note():  
+def delete_note():
     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
     noteId = note['noteId']
     note = Note.query.get(noteId)
@@ -34,4 +38,17 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+
+
+@views.route('/reorder-notes', methods=['POST'])
+@login_required
+def reorder_notes():
+    data = request.get_json()
+    order = data.get('order', []) if data else []
+    for position, note_id in enumerate(order):
+        note = Note.query.get(int(note_id))
+        if note and note.user_id == current_user.id:
+            note.position = position + 1
+    db.session.commit()
+    return jsonify({'success': True})
 
